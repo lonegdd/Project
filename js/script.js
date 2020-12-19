f0a1bc89a72d39f7eee97d0ad650039ea62f8d43
lonegdd <lonegdd.druid@gmail.com>
2020-12-19 13:17:28 +0300

    practica chast' pervaia

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log(1);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false,
+};
+
+let a = prompt('Один из последних просмотренных фильмов?',''),
+    b = prompt('На сколько оцените его?',''),
+    c = prompt('Один из последних просмотренных фильмов?',''),
+    d = prompt('На сколько оцените его?','');
+    personalMovieDB.movie[a] = b;
+    personalMovieDB.movie[c] = d;
+    console.log(personalMovieDB);
+
+    
